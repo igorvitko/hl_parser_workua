00b31cb86122f01c159385706deb99bec0014f39
IhorVitko <igor.vitko.74@gmail.com>
2021-07-25 23:13:25 +0300

    change spider file and change setting

--- hl_parser_workua/spiders/work_kharkiv.py
+++ hl_parser_workua/spiders/work_kharkiv.py
@@ -15,7 +15,6 @@ class WorkKharkivSpider(scrapy.Spider):
             result = {
                 'name': item.css('div b::text').get().strip(),
                 'age': None,
-                # 'link': card_uri,
                 'position': item.css('h2 a::text').get()
             }
 
@@ -23,14 +22,22 @@ class WorkKharkivSpider(scrapy.Spider):
                 'result': result
                 })
 
+        for page in response.css('ul.pagination li'):
+            if page.css('a::text').get() == 'Наступна':
+                yield response.follow(
+                    page.css('a::attr(href)').get(),
+                    self.parse
+                )
+
     def parse_person(self, response):
 
         age = response.css('div.card dd::text').get()[:2]
+        header = response.css('div.card > h2::text').get()
         description = ' '.join(response.css('div.card > p::text').getall())
-        description = ' '.join(description.split())
+        description = header + " " + ' '.join(description.split())
 
         people_info = response.meta['result']
-        people_info['description'] = description
         people_info['age'] = age
+        people_info['description'] = description
 
         yield people_info
